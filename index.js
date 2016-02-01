051c1ea6962ac14bfcde8a8298f179ed4e9a45ea
M.C. van den Boogaart <scola@kvdb.name>
2016-02-01 16:08:11 +0100

    Use dispatcher

--- index.js
+++ index.js
@@ -2,18 +2,19 @@
 
 const DI = require('@scola/di');
 
+const Dispatcher = require('./lib/dispatcher');
 const Messenger = require('./lib/messenger');
 const Model = require('./lib/model');
 
 class Module extends DI.Module {
   configure() {
-    this.inject(Model).with(
-      this.singleton(Messenger)
-    );
+    this.inject(Dispatcher)
+      .insertArgument(1, this.singleton(Messenger));
   }
 }
 
 module.exports = {
+  Dispatcher,
   Messenger,
   Model,
   Module
